383967b6e2a2ab88cf6c5a906e9785ad94996891
jiteshbilunia121 <jiteshbilunia1234@gmail.com>
2025-04-02 01:03:38 +0530

    Minor syntactical bug fixed in kafkabean template for ParkingEventProducer.java

--- src/main/java/org/example/parking_service/event/ParkingEventProducer.java
+++ src/main/java/org/example/parking_service/event/ParkingEventProducer.java
@@ -1,22 +1,26 @@
 package org.example.parking_service.event;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ParkingEventProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public ParkingEventProducer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    private final KafkaTemplate<String, CheckoutEvent> eventKafkaTemplate;
+    private final KafkaTemplate<String, String> stringKafkaTemplate;
+
+    public ParkingEventProducer(KafkaTemplate<String, CheckoutEvent> eventKafkaTemplate, KafkaTemplate<String, String> stringKafkaTemplate) {
+        this.eventKafkaTemplate = eventKafkaTemplate;
+        this.stringKafkaTemplate = stringKafkaTemplate;
     }
 
-    public void sendParkingSlotVacantEvent(String eventMessage) {
-        kafkaTemplate.send("parking.slot.occupied",eventMessage);
+    public void sendParkingSlotVacantEvent(CheckoutEvent event) {
+       eventKafkaTemplate.send("parking.slot.vacant", event);
     }
     public void sendParkingSlotOccupiedEvent(String eventMessage) {
-        kafkaTemplate.send("parking.slot.occupied",eventMessage);
+        stringKafkaTemplate.send("parking.slot.occupied",eventMessage);
     }
 }
